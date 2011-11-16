4b4c38e70df930307d4b743f79c673379abcf800
Daniel Barsotti <daniel.barsotti@liip.ch>
2011-11-16 09:22:09 +0100

    The script now waits for jackrabbit to reply when starting up

--- jackrabbit.sh
+++ jackrabbit.sh
@@ -59,7 +59,9 @@ LOGFILE=$BASEDIR/jackrabbit.log
 do_start() {
     if [ ! -f $PIDFILE ]; then
         cd $BASEDIR
-		nohup java $MEMORY $MANAGEMENT -jar $JACKRABBIT_JAR -h $JACKRABBIT_HOST -p $JACKRABBIT_PORT >> $LOGFILE 2>&1 & echo $! > $PIDFILE
+		    nohup java $MEMORY $MANAGEMENT -jar $JACKRABBIT_JAR -h $JACKRABBIT_HOST -p $JACKRABBIT_PORT >> $LOGFILE 2>&1 & echo $! > $PIDFILE
+        # Wait until the server is ready (from an idea of <chluehr> on FreeNode)
+        while [[ -z `curl -s "http://$JACKRABBIT_HOST:$JACKRABBIT_PORT"` ]] ; do sleep 1s; echo -n "."; done
         echo "Jackrabbit started"
     else
         echo "Jackrabbit is already running"
